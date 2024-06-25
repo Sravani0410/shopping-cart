748698b9ed07e4105653d153f66e56cacc71e625
sravani <sandhyapyla.8@gmail.com>
2024-06-25 17:20:41 +0530

    ui changes

--- shoppingcart/src/data/siteData.ts
+++ shoppingcart/src/data/siteData.ts
@@ -1,4 +1,4 @@
 export const metaData={
-    title:"Shopping-Cart",
+    title:"E-commerce shopping",
     description:"Product Management and Shopping-Cart you find on the web for web developer"
 }
