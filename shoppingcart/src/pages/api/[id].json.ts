b68e8d1e4601ef51f5870c25f8bb1f170a7165b9
sravani <sandhyapyla.8@gmail.com>
2024-06-25 07:24:38 +0530

    delete the product successfully

--- shoppingcart/src/pages/api/[id].json.ts
+++ shoppingcart/src/pages/api/[id].json.ts
@@ -1,17 +1,17 @@
 import type { APIRoute } from "astro";
-import type { Links } from "@/types/Links";
 
 export const DELETE: APIRoute = async ({ params }) => {
-  const id = params._id;
+  const { id } = params;
 
   try {
     const res = await fetch(`https://shopping-cart-backend-three.vercel.app/api/products/${id}`, {
       method: "DELETE",
     });
-const response=await res.json()
-console.log("delteres==>",res,response)
-
-
+    if (!res.ok) {
+      throw new Error("Error deleting link from external API");
+    }
+    const response = await res.json();
+    console.log("delete response =>", response);
     return new Response(JSON.stringify({ message: "Link deleted successfully", success: true }), {
       status: 200,
     });
